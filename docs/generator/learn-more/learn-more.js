147da632523dd82ebbc852f3f6af7db891388856
Parnell <psteide@ford.com>
2022-04-07 16:37:05 -0400

    Add functionality to learnmore page

--- docs/generator/learn-more/learn-more.js
+++ docs/generator/learn-more/learn-more.js
@@ -1,11 +1,11 @@
-// const wikiUrlTemplate = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${animal}`
 const queryString = window.location.search
 const queryParams = new URLSearchParams(queryString)
+const url = `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&limt=1&search=${queryParams.get("animal")}`
+const iframe = document.querySelector("iframe")
 
+fetch(url)
+.then(response => response.json())
+.then(parsedResponse => {
+    iframe.src = parsedResponse[3]
 
-
-// function addAnimalToUrl(queryParams.get("animal")){
-//     return `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${queryParams.get("animal")}`
-// }
-
-fetch( `https://en.wikipedia.org/w/api.php?action=opensearch&limt=1&search=${queryParams.get("animal")}`).then(response => console.log(response))
+})
