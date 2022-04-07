4f09389e18eb2d2f5d5452393faf5337b1e4adc1
Parnell <psteide@ford.com>
2022-04-07 09:26:31 -0400

    Add card styling

--- site/index.js
+++ site/index.js
@@ -1,19 +1,25 @@
 const animalApiUrl = "https://zoo-animal-api.herokuapp.com/animals/rand/1"
 const animalHook = document.querySelector(".animals")
 
+function lowerCaseFirstLetter(string){
+    return string.charAt(0).toLowerCase() + string.slice(1)
+}
+
+
+
 fetch(animalApiUrl)
 .then(response => response.json())
 .then(animals =>animals.forEach(animal =>{
     const li = document.createElement("li")
-    li.classList.add("animal-card")
+    li.classList.add("animal-cards")
     li.innerHTML =`
-        <div>
+        <div class = "animal-card">
         <img src = "${animal.image_link}" alt = ${animal.name}>
         <div>
-         <p>${animal.name} <span> (${animal.latin_name}) </span} </p>
-         <p> ${animal.habitat} </p>
-         <p> ${animal.diet} </p>
-         <p> This animal can be found: ${animal.geo_range} </p>
+         <p class = "animal-name">${animal.name} <span> (${animal.latin_name}) </span} </p>
+         <p> Lives in ${lowerCaseFirstLetter(animal.habitat)} </p>
+         <p> Eats: ${animal.diet} </p>
+         <p> Where in the world can this animal be found: ${animal.geo_range} </p>
         </div>
         </div> 
     `
